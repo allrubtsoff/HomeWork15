5629b2ad5b8d861700dc624d1cc78640014a2704
allrubtsoff <allrubtsooff@yandex.ru>
2021-04-23 10:02:05 +0300

    Добавьте файлы проекта.

---
+++ HomeWork15.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+void print()
+{
+    int a = 0;
+    int b = 10;
+    for (int i = a; i <= b; i++)
+    {
+        if (i % 2 != 0)
+            std::cout << i << "\n";
+    }
+
+}
+
+int main()
+{
+    int a = 0;
+    const int b = 10;
+    for (int i = a; i <= b; i++) 
+    {
+        if (i % 2 == 0)
+        std::cout << i << "\n";
+    }
+    
+    print();
+   
+}
+
